846faf75f4fd41e1cc083c690e4f1a77c5175322
Amirali Farazmand <75451772+AmiraliFarazmand@users.noreply.github.com>
2025-04-06 17:01:07 +0330

    feat(db): add CRUD abalities to db package

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/AmiraliFarazmand/PTC_Task/internal/db"
+import (
+	"fmt"
+
+	"github.com/AmiraliFarazmand/PTC_Task/internal/db"
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
 
 func main() {
-	db.ConnectDB()
+	db.ConnectDB("mongodb://localhost:27017", "ParsTasmimDB")
+	userCollection := db.GetCollection("Users")
+	// db.InsertIntoCollection(userCollection, bson.M{"username": "sdfsf", "password": "sdfsdf"})
+	found := db.FindInstance(userCollection, bson.M{"username":"sdfsf"})
+	fmt.Println(found)
+	
 }
--- internal/db/db.go
+++ internal/db/db.go
@@ -5,21 +5,39 @@ import (
 	"fmt"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
-var Database  mongo.Database
-func ConnectDB() {
 
+var DB *mongo.Database
+
+func ConnectDB(uri, dbName string) {
 	_, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
-	client, _ := mongo.Connect(options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, _ := mongo.Connect(options.Client().ApplyURI(uri))
+	DB = client.Database(dbName)
+}
 
-	Database := client.Database("ParsTasmimDB")
-	usersCollection :=Database.Collection("Users")
-	fmt.Println(usersCollection)
-	res, _ := usersCollection.InsertOne(context.Background(), bson.M{"username": "rnduser", "password": "idk"})
+func GetCollection(collectionName string) *mongo.Collection {
+	return DB.Collection(collectionName)
+}
+
+func InsertIntoCollection(collection *mongo.Collection, instance bson.M) {
+	res, _ := collection.InsertOne(context.Background(), instance)
 	id := res.InsertedID
 	fmt.Println(id)
 }
+
+func FindInstance(collection *mongo.Collection, instance bson.M) *mongo.SingleResult {
+	return collection.FindOne(context.Background(), instance)
+}
+
+func DeleteInstance(collection *mongo.Collection, instance bson.M) {
+	collection.DeleteOne(context.Background(), instance)
+}
+
+func UpdateInstance(collection *mongo.Collection, filter bson.M, update bson.M) {
+	collection.UpdateOne(context.Background(), filter, bson.M{"$set": update})
+}
+
