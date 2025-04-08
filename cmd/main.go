d63decb5fb0a7470842699655b24f378e2343f32
Amirali Farazmand <75451772+AmiraliFarazmand@users.noreply.github.com>
2025-04-08 14:34:43 +0330

    feat (purchase): add concurrent function to cancel old unpaid purchases

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"sync"
+
 	api "github.com/AmiraliFarazmand/PTC_Task/internal/API"
 	"github.com/AmiraliFarazmand/PTC_Task/internal/db"
+	"github.com/AmiraliFarazmand/PTC_Task/internal/purchase"
 )
 
 func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		purchase.CancelUnpaidPurchases()
+	}()
+
 	db.ConnectDB("mongodb://localhost:27017", "ParsTasmimDB")
 	r := api.SetupRouter()
 	r.Run()
 
+	wg.Wait() // Wait for the goroutine to finish
 }
--- internal/purchase/purchase.go
+++ internal/purchase/purchase.go
@@ -1,18 +1,18 @@
 package purchase
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
-	"github.com/AmiraliFarazmand/PTC_Task/internal/utils"
 	"github.com/AmiraliFarazmand/PTC_Task/internal/auth"
 	"github.com/AmiraliFarazmand/PTC_Task/internal/db"
+	"github.com/AmiraliFarazmand/PTC_Task/internal/utils"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type Purchase struct {
+	ID        bson.ObjectID `bson:"_id"`
 	UserID    bson.ObjectID `bson:"user_id"`
 	Amount    int           `bson:"amount"`
 	CreatedAt time.Time     `bson:"created_at"`
@@ -29,10 +29,10 @@ type PurchaseRequest struct {
 func CreatePurchase(c *gin.Context) {
 	user, _ := c.Get("user")
 	userObj, ok := user.(auth.User)
-    if !ok {
-        utils.RespondWithError(c, http.StatusInternalServerError, "Failed to retrieve user")
-        return
-    }
+	if !ok {
+		utils.RespondWithError(c, http.StatusInternalServerError, "Failed to retrieve user")
+		return
+	}
 
 	var req PurchaseRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
@@ -40,24 +40,21 @@ func CreatePurchase(c *gin.Context) {
 	}
 	// fmt.Printf("****%+v\n", user)
 
-    purchase := Purchase{
-        UserID:    userObj.ID, // Use the ID field from the user object
-        Amount:    req.Amount,
-        CreatedAt: time.Now(),
-        Status:    "pending",
-        PaymentID: "",
-        Address:   req.Address,
+	purchase := Purchase{
+		ID:  bson.NewObjectID(),
+		UserID:    userObj.ID, // Use the ID field from the user object
+		Amount:    req.Amount,
+		CreatedAt: time.Now(),
+		Status:    "pending",
+		PaymentID: "",
+		Address:   req.Address,
 	}
 
-    _, err := db.PurchaseCollection.InsertOne(c.Request.Context(), purchase)
-    if err != nil {
-        utils.RespondWithError(c, http.StatusInternalServerError, "Failed to create purchase")
-        return
-    }
+	_, err := db.PurchaseCollection.InsertOne(c.Request.Context(), purchase)
+	if err != nil {
+		utils.RespondWithError(c, http.StatusInternalServerError, "Failed to create purchase")
+		return
+	}
 	c.JSON(http.StatusCreated, gin.H{"message": "Purchase created successfully"})
-    fmt.Printf("%+v\n", purchase)
-
-	// call a function to process the purchase in a limited time otherwise drop it
-
+	// fmt.Printf("%+v\n", purchase)
 }
-
