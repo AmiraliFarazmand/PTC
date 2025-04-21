8885875c1ff4930b0903326eb0d0fe786988c9e0
amirfzd <amiralifm1407@gmail.com>
2025-04-21 19:53:03 +0330

    chore: main supports GIN functionalities but not connected to Zeebe

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,8 @@ import (
 	"github.com/AmiraliFarazmand/PTC_Task/internal/adapters/zeebe"
 	"github.com/AmiraliFarazmand/PTC_Task/internal/utils"
 	"github.com/camunda-community-hub/zeebe-client-go/v8/pkg/worker"
-	// "github.com/AmiraliFarazmand/PTC_Task/internal/core/app"
-	// "github.com/AmiraliFarazmand/PTC_Task/internal/adapters/http"
+	"github.com/AmiraliFarazmand/PTC_Task/internal/core/app"
+	"github.com/AmiraliFarazmand/PTC_Task/internal/adapters/http"
 )
 
 func main() {
@@ -18,11 +18,11 @@ func main() {
 	// Create repositories
 	dbName, _ := utils.ReadEnv("DB_NAME")
 	userRepo := db.NewMongoUserRepository(client.Database(dbName).Collection("Users"))
-	// purchaseRepo := db.NewMongoPurchaseRepository(client.Database("ParsTasmimDB").Collection("Purchases"))
+	purchaseRepo := db.NewMongoPurchaseRepository(client.Database("ParsTasmimDB").Collection("Purchases"))
 
 	// Initialize services
-	// userService:= app.InitializeUserService(userRepo)
-	// purchaseService := app.InitializePurchaseService(purchaseRepo)
+	userService:= app.InitializeUserService(userRepo)
+	purchaseService := app.InitializePurchaseService(purchaseRepo)
 	zeebeClient := zeebe.NewZeebeClient()
 	defer zeebe.MustCloseClient(zeebeClient)
 	// Deploy BPMN process
@@ -34,11 +34,9 @@ func main() {
 	createUserJobWorker = zeebe.CreateUserWorker(zeebeClient, userRepo)
 	defer validateJobWorker.Close()
 	defer createUserJobWorker.Close()
-	//create instance of the process
-	zeebe.MustStartProcessInstance(zeebeClient, "userNW", "password")
 	// Initialize and start HTTP server
-	// server := http.InitializeHTTPServer(purchaseService, userService)
-	// server.Start()
+	server := http.InitializeHTTPServer(purchaseService, userService)
+	server.Start()
 	select {}
 }
 
