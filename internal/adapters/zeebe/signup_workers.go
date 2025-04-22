94f53e6d2740804186bb8c9c3c5c95867d8fd813
amirfzd <amiralifm1407@gmail.com>
2025-04-22 14:04:52 +0330

    chore: put worker functions in a single file

--- internal/adapters/zeebe/signup_workers.go
+++ internal/adapters/zeebe/signup_workers.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"time"
 
+	"github.com/AmiraliFarazmand/PTC_Task/internal/adapters/db"
+	"github.com/AmiraliFarazmand/PTC_Task/internal/core/domain"
 	"github.com/camunda-community-hub/zeebe-client-go/v8/pkg/entities"
 	"github.com/camunda-community-hub/zeebe-client-go/v8/pkg/worker"
 	"github.com/camunda-community-hub/zeebe-client-go/v8/pkg/zbc"
@@ -43,4 +45,35 @@ func ValidateCredentialsWorker(client zbc.Client) worker.JobWorker{
 	// defer jobWorker.Close()
 	log.Println("####Ended validate-credentials worker")
 	return jobWorker
+}
+
+
+func CreateUserWorker(client zbc.Client, userRepo *db.MongoUserRepository) worker.JobWorker {
+    jobWorker := client.NewJobWorker().
+        JobType("create-user").
+        Handler(func(jobClient worker.JobClient, job entities.Job) {
+            vars,_ := job.GetVariablesAsMap()
+            username := vars["username"].(string)
+            password := vars["password"].(string)
+            // isValid := vars["isValid"].(bool)
+
+            // Create user in the database
+            err := userRepo.Create(domain.User{Username: username, Password: password})
+            if err != nil {
+                log.Printf("###failed to create user: %v", err)
+                return
+            }
+
+            // Complete the job
+            _, err = jobClient.NewCompleteJobCommand().
+                JobKey(job.GetKey()).
+                Send(context.Background())
+            if err != nil {
+                log.Printf("###Failed to complete job: %v", err)
+            }
+        }).
+        Open()
+        log.Println("###CreateUserWorker started")
+    // defer jobWorker.Close()
+    return jobWorker 
 }
